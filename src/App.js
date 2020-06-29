b9015e501be3c45cf57b9b74f0a8557a7658226d
Leon <tianyi@iotum.com>
2020-06-29 10:59:24 -0400

    add detection

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import bowser from "bowser";
+import Platform from "platform";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.PureComponent {
+    constructor() {
+        super();
+
+        this.state = {
+            isOnMac: 'OS X' === Platform.os.family,
+            isOnIos: bowser.ios === true,
+            isTouchDevice: 'ontouchstart' in window,
+            isMaxTouch: navigator.maxTouchPoints > 1
+        };
+    }
+
+  render() {
+    return (
+        <div className="App">
+          <header className="App-header">
+            <p>
+                <p>
+                    isOnMac: 'OS X' === Platform.os.family
+                    <br/>
+                    Result: {this.state.isOnMac.toString()}
+                </p>
+
+                <p>
+                    bowser.ios === true
+                    <br/>
+                    Result: {this.state.isOnIos.toString()}
+                </p>
+
+                <p>
+                    isTouchDevice: 'ontouchstart' in window
+                    <br/>
+                    Result: {this.state.isTouchDevice.toString()}
+                </p>
+
+                <p>
+                    isMaxTouch: navigator.maxTouchPoints > 1
+                    <br/>
+                    Result: {this.state.isMaxTouch.toString()}
+                </p>
+            </p>
+          </header>
+        </div>
+    );
+  }
 }
 
 export default App;
